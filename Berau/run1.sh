7098dc01cb9e90f0fee267b7fece6216119f8159
Takashi NAKAMURA <nakamura.t.av@m.titech.ac.jp>
2019-11-01 22:03:08 +0900

    Modified: Berau project and input files

--- Berau/run1.sh
+++ Berau/run1.sh
@@ -1,2 +1,2 @@
-mpirun -use-hwthread-cpus -np 10 ./coawstG.exe ocean_berau1.in
+mpirun -use-hwthread-cpus -np 11 ./coawstG.exe ocean_berau1.in
 #
---
+++ Berau/run2.sh
@@ -0,0 +1,2 @@
+mpirun -use-hwthread-cpus -np 11 ./coawstG.exe ocean_berau2.in
+#
