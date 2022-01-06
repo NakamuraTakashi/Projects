625f4a26a81d57595c8062f399a45c7c0c941623
Takashi NAKAMURA <nakamura.t.av@m.titech.ac.jp>
2022-01-06 22:43:08 +0900

    Add: projects for online/offline float/biology

---
+++ Yaeyama2_3_201004-_float/qsubT2.sh
@@ -0,0 +1,3 @@
+#qsub -g tga-NakamuLab jobT3_gnu.sh
+qsub -g tga-NakamuLab jobT2_intel.sh
+#qsub -g tga-NakamuLab jobT3_pgi.sh
---
+++ Yaeyama2_3_201004-_offline/run_Y2.sh
@@ -0,0 +1,3 @@
+mpirun -use-hwthread-cpus -np 10 ./coawstM.exe ocean_Y2_offline.in
+#mpirun -use-hwthread-cpus -np 10 ./coawstM.exe ocean_Y2_online_float.in
+#
