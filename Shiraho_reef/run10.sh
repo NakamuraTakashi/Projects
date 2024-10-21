7cf1513b62c84dae82d2fbdecc93b1b6ccd01c24
Takashi NAKAMURA <nakamura.t.av@m.titech.ac.jp>
2024-10-21 22:39:41 +0900

    Update Shiraho_reef project for adopting WSL2

--- Shiraho_reef/run10.sh
+++ Shiraho_reef/run10.sh
@@ -1,3 +1,3 @@
 export HDF5_DISABLE_VERSION_CHECK=1
-mpirun -use-hwthread-cpus -np 7 ./coawstM.exe coupling_shiraho_reef_10.in
+mpirun -use-hwthread-cpus -np 7 ./coawstM coupling_shiraho_reef_10.in
 ##
--- Shiraho_reef/shiraho_reef.h
+++ Shiraho_reef/shiraho_reef.h
@@ -194,40 +194,45 @@
 
 /***  Biological model options. (Original CPP flags) ***/
 
-#define REEF_ECOSYS
+/*#define REEF_ECOSYS*/
+
+#if defined REEF_ECOSYS || defined SEDIMENT
+# define ANA_TOBC_BIO  /*Original CPP flag */
+# define ANA_TOBC_SED   /*Original CPP flag */
+/*# define BIO_VPROFILE_YAEYAMA*/   /*Original CPP flag */
+#endif
 
 #if defined REEF_ECOSYS
 # define BIOLOGY
+# define DIAGNOSTICS_BIO
 # define ANA_BIOLOGY
-/*# define ANA_TOBC_BIO*/   /*Original CPP flag */
 
 /* compartments */
-/*# define ORGANIC_MATTER*/
+# define ORGANIC_MATTER
 /*# define CARBON_ISOTOPE*/
-/*# define NUTRIENTS*/
+# define NUTRIENTS
 
 # define CORAL_POLYP  /* USE coral module */
 # define SEAGRASS     /* USE seagrass module */
 # define MACROALGAE        /* USE algae module  */
 # define SEDIMENT_ECOSYS        /* USE sedecosys module  */
-# if defined SEDIMENT_ECOSYS
-#  define SEDIMENT_EMPIRICAL     /* USE empirical sediment module  */
-# endif
 
 # if defined ORGANIC_MATTER
 #  define FOODWEB      /* USE foodweb module */
 # endif
 # define AIR_SEA_GAS_EXCHANGE
 
+/*# define DYNAMIC_COVERAGE*/ /* yt_edit not yet implemented in coawst */
+
+
 /*** Coral Polyp model options. ***/
 # if defined CORAL_POLYP
 #  define CORAL_ZOOXANTHELLAE
-#  define CORAL_PHOTOINHIBITION
-/*#  define CORAL_MUCUS*/           /*Mucus release from coral */
+#  define CORAL_MUCUS           /*Mucus release from coral */
 #  if defined ORGANIC_MATTER
-/*#   define CORAL_INGESTION*/
+#   define CORAL_INGESTION
 #  endif
-#  define CORAL_SIZE_DYNAMICS
+/*#  define CORAL_SIZE_DYNAMICS*/
 #  if defined CARBON_ISOTOPE
 #   define CORAL_CARBON_ISOTOPE
 /*#   define CORAL_NONE_CO2_EQ*/
@@ -238,5 +243,37 @@
 /*#  define CORAL_BORON_ISOTOPE*/
 # endif
 
-#endif
 
+/*** Seagrass model options. ***/
+# if defined SEAGRASS
+#  if defined NUTRIENTS
+#   define SEAGRASS_LEAF_NUTRIENT_UPTAKE
+#  endif
+#  if defined SEDIMENT_ECOSYS
+/*#   define SEAGRASS_ROOT_CARBON_OXYGEN_EXCHANGE*/
+#  endif
+#  if defined NUTRIENTS && defined SEDIMENT_ECOSYS
+#   define SEAGRASS_ROOT_NUTRIENT_UPTAKE
+#  endif
+#  if defined ORGANIC_MATTER
+#   define SEAGRASS_LEAF_POM
+#   if defined SEDIMENT_ECOSYS
+#    define SEAGRASS_ROOT_POM
+#   endif
+#  endif
+# endif
+
+
+/*** Sediment model options. ***/
+# if defined SEDIMENT_ECOSYS  /* Masa_edits */
+/*#  define SEDIMENT_EMPIRICAL*/     /* USE empirical sediment module  */
+#  define SEDECO_CLOSED_BOTTOM_DIFFUSION_BOUNDARY /* closed boundary condition at the bottom sediment layer */
+#  define SULFATE      /* For sulfate reduction in sediment */
+/*#  define SEDECO_BURIAL*/    /* For Burial term in sediment transport (massbalance) */
+#  define SEDECO_ADVECTION
+#  define ORGANIC_MATTER
+#  define NUTRIENTS
+# endif
+
+
+#endif
